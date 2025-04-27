ad26037a150c28dc405009e22e4159d30ca5e285
ZorgoX <almin.cisa@gmail.com>
2025-04-27 20:44:51 +0200

    Update main.js

--- Js/main.js
+++ Js/main.js
@@ -1,3 +1,5 @@
-// ZorgoX JavaScript File
-
-console.log("Benvenuto su ZorgoX!");
+document.getElementById("missionButton").addEventListener("click", function() {
+  const sound = document.getElementById("alienSound");
+  sound.play();
+  alert("Missione ZorgoX: Stabilire una connessione interstellare sicura.");
+});
